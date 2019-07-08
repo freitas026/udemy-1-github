ca29b8c824970c9c1101b45a15c7ae69e2c630d4
freitas26 <freitasdaval@hotmail.com>
2019-07-08 16:42:38 -0300

    Acrescentada msg

--- src/udemycourse/Udemycourse.java
+++ src/udemycourse/Udemycourse.java
@@ -16,6 +16,9 @@ public class Udemycourse {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+            System.out.println("Bom dia!");
+
     }
     
 }
